4cdcc9fcd0a21bd45b7fdae37b9aead40e3ffd4f
Shihara1020 <eranjan1020@gmail.com>
2024-05-26 17:59:49 +0530

    update gallary section

---
+++ button.js
@@ -0,0 +1,3 @@
+document.getElementById('google-doc-button').onclick = function() {
+    window.open('https://docs.google.com/forms/d/e/1FAIpQLSd-pwqGxP1QO50xaqP3Ep82-eIoQ8x0phDxRG6LR_sAtb6X4w/viewform?usp=sf_link', '_blank');
+};
