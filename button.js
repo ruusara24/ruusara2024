de14b757d0cced3dc6ae05505aee234ef3c8f51b
Shihara1020 <eranjan1020@gmail.com>
2024-06-24 18:53:51 +0530

    add submit button

--- button.js
+++ button.js
@@ -1,3 +1,6 @@
 document.getElementById('google-doc-button').onclick = function() {
     window.open('https://docs.google.com/forms/d/e/1FAIpQLSd-pwqGxP1QO50xaqP3Ep82-eIoQ8x0phDxRG6LR_sAtb6X4w/viewform?usp=sf_link', '_blank');
 };
+document.getElementById('google-doc-button2').onclick = function() {
+    window.open('https://docs.google.com/forms/d/e/1FAIpQLScYZROIUTI44Bwtf-AejU7WTgaDq4ENMfzWaOvzh02ZOEGjAg/viewform?usp=sf_link', '_blank');
+};
